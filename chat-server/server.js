3ddf9504b351708bc81b6fa0b1855c4045bd1cc6
Bastian Alzate <bastianmurilloalzate@gmail.com>
2023-04-29 19:53:14 -0500

    config initial firebase-admin sdk

---
+++ chat-server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const admin = require('firebase-admin');
+
+// Configurar Firebase Admin SDK
+const serviceAccount = require('./serviceAccountKey.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
+
+// Configurar servidor HTTP con Express
+const app = express();
+const server = http.createServer(app);
+
+// Configurar Socket.IO para comunicación en tiempo real
+const io = socketIO(server);
+io.on('connection', socket => {
+  console.log('Usuario conectado');
+
+  socket.on('message', message => {
+    console.log('Mensaje recibido:', message);
+    // Guardar mensaje en Firestore
+    db.collection('messages').doc(message.id).set(message);
+    // Emitir mensaje a todos los clientes conectados
+    io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado');
+  });
+});
+
+// Iniciar servidor HTTP
+const port = 5000;
+server.listen(port, () => {
+  console.log(`Servidor iniciado en http://localhost:${port}`);
+});
