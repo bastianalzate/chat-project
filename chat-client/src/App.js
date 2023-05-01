d75aa18d574e3cc693cfe7dd85175e9883a5f3ce
Bastian Alzate <bastianmurilloalzate@gmail.com>
2023-05-01 10:43:42 -0500

    add react-router-dom v5

--- chat-client/src/App.js
+++ chat-client/src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
 import './App.css';
 import Chat from './components/Chat';
 
 function App() {
 
   return (
-    <div className="App">
-      <Chat />
-    </div>
+    <Switch>
+      <Route exact path="/" component={Chat} />
+      {/* <Route path="/about" component={About} /> */}
+      {/* <Route path="/contact" component={Contact} /> */}
+      {/* <Route component={NotFound} /> */}
+    </Switch>
   );
 }
 
